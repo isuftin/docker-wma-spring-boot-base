bc23b344559b596e02ea5f00b4d673ad6d417865
Zack Moore <zmoore@usgs.gov>
2018-08-14 13:07:28 -0500

    Tweaks to the healthcheck and travis check based on feedback from Ivan.

--- launch-app.sh
+++ launch-app.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# NOTE: This is needed because if JAVA_OPTIONS are overidden via docker-compose (either
+# with an ENV file or ENV section in the compose file) and you want to include references
+# to environment variables within the container then you need to escape those with double
+# $$ which ends up putting your variables as strings into $JAVA_OPTIONS so we need to use
+# eval to subtitute in the proper values once the container is running.
 if [ -z "$JAVA_OPTIONS" ]; then
     JAVA_OPTIONS=$(eval echo $JAVA_OPTIONS)
 else
