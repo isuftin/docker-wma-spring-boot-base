affd719799ce986555e74859388072acb80a01fe
Zack Moore <zmoore@usgs.gov>
2018-08-15 11:59:45 -0500

    Better error logging related to the keystore.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,22 @@ keystoreLocation=${keystoreLocation:?}
 keystoreSSLKey=${keystoreSSLKey:?}
 keystorePassword=${keystorePassword:?}
 
+# Because I am the root user, I cannot write to the system java keystore.
+# Therefore I copy the Java keystore to a local area. The source location
+# for the Java keystore is /etc/ssl/certs/java/cacerts for this image
+# (openjdk:8-jdk-alpine) but may differ on other images
+keytool -importkeystore -srckeystore /etc/ssl/certs/java/cacerts -srcstorepass $JAVA_TRUSTSTORE_PASS -destkeystore $JAVA_TRUSTSTORE -deststorepass $JAVA_TRUSTSTORE_PASS
+
 if [ -n "${TOMCAT_CERT_PATH}" ] && [ -n "${TOMCAT_KEY_PATH}" ] && [ -f "${TOMCAT_CERT_PATH}" ] && [ -f "${TOMCAT_KEY_PATH}" ]; then
   # If the previous keystore location exists, remove it as I will create a new file there
   if [ -f $keystoreLocation ]; then
     rm $keystoreLocation
   fi
 
-  # Because I am the root user, I cannot write to the system java keystore.
-  # Therefore I copy the Java keystore to a local area. The source location
-  # for the Java keystore is /etc/ssl/certs/java/cacerts for this image
-  # (openjdk:8-jdk-alpine) but may differ on other images
-  keytool -importkeystore -srckeystore /etc/ssl/certs/java/cacerts -srcstorepass $JAVA_TRUSTSTORE_PASS -destkeystore $JAVA_TRUSTSTORE -deststorepass $JAVA_TRUSTSTORE_PASS
   openssl pkcs12 -export -in $TOMCAT_CERT_PATH -inkey $TOMCAT_KEY_PATH -name $keystoreSSLKey -out $HOME/tomcat.pkcs12 -password pass:$keystorePassword
   keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -deststoretype PKCS12 -srckeystore $HOME/tomcat.pkcs12 -srcstorepass $keystorePassword -srcstoretype PKCS12 -noprompt
+else
+  echo "WARNING: Tomcat cert and/or key not found at '$TOMCAT_CERT_PATH' and/or '$TOMCAT_KEY_PATH'."
 fi
 
 if [ -n "${CERT_IMPORT_DIRECTORY}" ] && [ -d "${CERT_IMPORT_DIRECTORY}" ]; then
@@ -33,6 +36,8 @@ if [ -n "${CERT_IMPORT_DIRECTORY}" ] && [ -d "${CERT_IMPORT_DIRECTORY}" ]; then
     fi
 
   done
+else
+  echo "WARNING: Cert import directory not found at '$CERT_IMPORT_DIRECTORY'. No additional certs will be imported into '$JAVA_TRUSTSTORE'."
 fi
 
 launch_app="${HOME}/launch-app.sh"
